3be1fbf1659fb23424a8d7e1f5f15a61f1e66df2
Coen <coenstange@me.com>
2017-09-21 11:27:02 +0200

    + save token in preferences

--- app/src/main/java/com/example/routin/fhictcompanion/LoginActivity.java
+++ app/src/main/java/com/example/routin/fhictcompanion/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.routin.fhictcompanion;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,12 +17,35 @@ public class LoginActivity extends AppCompatActivity implements TokenFragment.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        loadToken();
     }
 
 
     @Override
     public void onFragmentInteraction(String token) {
+        saveToken(token);
+        startIntent();
+    }
+
+    private void saveToken(String token) {
+        TokenSingleton.getInstance().setToken(token);
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("token", token);
+        editor.commit();
+    }
+
+    private void loadToken() {
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String token = sharedPref.getString("token", "");
+        if (token != "") {
+            TokenSingleton.getInstance().setToken(token);
+            startIntent();
+        }
+    }
+
+    private void startIntent(){
         // LOGGED IN YEAH
         // Intent(FirstActivity, SecondActivity.class)
         Intent intent = new Intent(this, MainActivity.class);
---
+++ app/src/main/java/com/example/routin/fhictcompanion/TokenSingleton.java
@@ -0,0 +1,27 @@
+package com.example.routin.fhictcompanion;
+
+/**
+ * Created by coen on 21/09/2017.
+ */
+
+class TokenSingleton {
+    private static final TokenSingleton ourInstance = new TokenSingleton();
+
+    static TokenSingleton getInstance() {
+        return ourInstance;
+    }
+
+    private TokenSingleton() {
+
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    private String token;
+}
